05b45371dffb21d5661fb469172e1438c06759f5
tanvirch0750 <tanvir0750@yahoo.com>
2023-10-19 22:50:33 +0600

    add api url

--- src/helpers/config/envConfig.ts
+++ src/helpers/config/envConfig.ts
@@ -1,3 +1,6 @@
 export const getBaseUrl = (): string => {
-  return process.env.NEXT_PUBLIC_API_BASE_URL || 'http://localhost:5001/api/v1';
+  return (
+    process.env.NEXT_PUBLIC_API_BASE_URL ||
+    'https://house-crafters.vercel.app/api/v1'
+  );
 };
