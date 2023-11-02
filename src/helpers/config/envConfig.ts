7daf9df52638e495ff9d738b0cf1ffc5a88e16e7
tanvirch0750 <tanvir0750@yahoo.com>
2023-11-02 22:34:20 +0600

    change base backend url

--- src/helpers/config/envConfig.ts
+++ src/helpers/config/envConfig.ts
@@ -1,5 +1,9 @@
 export const getBaseUrl = (): string => {
-  return process.env.NEXT_PUBLIC_API_BASE_URL || 'http://localhost:5001/api/v1';
+  return (
+    process.env.NEXT_PUBLIC_API_BASE_URL ||
+    'https://house-crafters.vercel.app/api/v1'
+  );
 };
 
 // 'https://house-crafters.vercel.app/api/v1'
+// 'http://localhost:5001/api/v1'
